a325362dc7ca09f15b7d0c68aa647d3942a6c724
surendrafed <95557837+chicken3301@users.noreply.github.com>
2024-10-26 16:11:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('hamburger').onclick = function() {
+    var menu = document.getElementById('menu');
+    menu.classList.toggle('visible'); // Toggle visibility of the menu
+};
+
